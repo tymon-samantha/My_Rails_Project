c02f38cc891c6f0c5fe3399e8e8b016195531597
tymon.samantha <tymon.samantha@gmail.com>
2016-09-05 04:36:27 -0400

    4.1 adjusted nav buttons styling. added path

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   resources :products
+
+  get 'static_pages/products'
+
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
+
   root 'static_pages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
