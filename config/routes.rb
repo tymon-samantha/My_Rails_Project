00d6bf1438b778cecb9552b722478ce803d1e3fa
tymon.samantha <tymon.samantha@gmail.com>
2016-09-07 00:47:07 -0400

    4.3 routed to landing page

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
-  root 'products#index'
+  root 'static_pages#landing_page'
 end
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
