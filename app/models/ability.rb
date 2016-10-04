f00a114e7799afaff1f69e62144fcbf0ff49e1e9
tymon.samantha <tymon.samantha@gmail.com>
2016-10-04 02:05:35 -0400

    5.11 correct where user restrictions were input in models

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,13 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.admin?
+      can :manage, :all
+    else
+      cannot :destroy, Comment
+      can :read, :all
+      can :manage, User, id: user.id
+    end
   end
 
 end
