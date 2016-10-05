58c87d67a783abb546c335e156cb42e7003a5769
tymon.samantha <tymon.samantha@gmail.com>
2016-10-05 01:05:26 -0400

    5.11 Automated email upon sign up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -14,4 +14,10 @@ class UserMailer < ApplicationMailer
     @message = params[:message]
     UserMailer.contact_form(@email, @name, @message).deliver_now
   end
+
+  def welcome(user)
+    @appname = "Bike Berlin"
+    mail( :to => user.email,
+      :subject => "Welcome to #{@appname}!")
+  end
 end
