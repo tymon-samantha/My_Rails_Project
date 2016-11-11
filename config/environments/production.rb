daa50dbdaf3f4065bb3f767bf84ff34f23fb3019
tymon.samantha <tymon.samantha@gmail.com>
2016-11-11 00:09:30 -0500

    6.13 actioncable not working in productin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,8 +37,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.web_socket_server_url = "wss://samtymrailsproject.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://samtymrailsproject.herokuapp.com', 'http://samtymrailsproject.herokuapp.com']
+  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -86,4 +85,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.web_socket_server_url = "wss://samtymrailsproject.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://samtymrailsproject.herokuapp.com', 'http://samtymrailsproject.herokuapp.com']
 end
